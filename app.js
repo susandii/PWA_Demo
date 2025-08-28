be016ad72f755211e10f07391fd66623ff373c13
susandii <susandimayadunne@gmail.com>
2025-08-28 09:19:47 +0530

    second commit

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 document.getElementById('ping').addEventListener("click", function(){
-    document.getElementById('out').textContent="JS is working";
+    document.getElementById('out').textContent="Java Script is working";
 })
+
+
+//helper file
+//runs in the baclground, seperate from the main page
+//offline performance, overall performance, push notifications
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('sw.js')
+    .then(()=> console.log('service Worker registerred'))
+    .catch(err => console.log('SW registration failed', err));
+
+}
 
